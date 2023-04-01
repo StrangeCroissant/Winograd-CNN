93a9c8f1fa3185f7b28732d237e1e33a856603c2
StranceCroissant <metalocraven@gmail.com>
2023-04-01 18:00:23 +0300

    activations

--- activation.py
+++ activation.py
@@ -1,7 +1,14 @@
 import numpy as np 
+import layer
 
+class Activation(layer):
+  def __init__(self,activation,activation_prime):
+    self.activation = activation
+    self.activcation_prime = activation_prime
 
+    def forward(self, input):
+        self.input = input
+        return self.activation(self.input)
 
-def softmax(x):
-  return np.exp(x)/np.sum(np.exp(x),axis=0)
-
+    def backward(self, output_gradient, learning_rate):
+        return np.multiply(output_gradient, self.activation_prime(self.input))
