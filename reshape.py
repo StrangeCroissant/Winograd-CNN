ba4868f37a6aca8f6ca37bee8907beb2c5291a60
StrangeCroissant <metalocraven@gmail.com>
2023-04-01 23:35:21 +0300

    how to cnn

--- reshape.py
+++ reshape.py
@@ -1,17 +1,17 @@
 import numpy as np
 from layer import Layer
 
+
 class Reshape():
-    def __init__(selfm,imput_shape,output_shape):
-        #shape of input and output
+    def __init__(self, input_shape, output_shape):
+        # shape of input and output
         self.input_shape = input_shape
         self.output_shape = output_shape
 
-    def forward(self,image):
-        #reshape input to output
-        return np.reshape(input,self.output_shape)
+    def forward(self, image):
+        # reshape input to output
+        return np.reshape(image, self.output_shape)
 
-    def backward(self,output_shape,lr):
-        #reshape output to input
-        return np.reshape(output_grad,self.input_shape)
-
+    def backward(self, output_shape, lr):
+        # reshape output to input
+        return np.reshape(output_grad, self.input_shape)
