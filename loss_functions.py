e9131ffa993fce74a3af3d2878f1f8e8d250d332
StrangeCroissant <metalocraven@gmail.com>
2023-04-02 10:55:19 +0300

    commit

--- loss_functions.py
+++ loss_functions.py
@@ -8,10 +8,15 @@ import numpy as np
         p_i = softmax probability of the ith class or y_pred
 """
 
-def cross_entropy(y_true,y_pred):
+
+def cross_entropy(y_true, y_pred):
 
     loss = -np.sum(y_true*np.log(y_pred))
     return loss/float(y_pred.shape[0])
+
+
+def binary_cross_entropy_prime(y_true, y_pred):
+    return ((1 - y_true) / (1 - y_pred) - y_true / y_pred) / np.size(y_true)
 
 
 """
@@ -19,7 +24,9 @@ def cross_entropy(y_true,y_pred):
 
         mse = - SUM[i=1 -> n]{(y_true_i - y_pred)^2}/n for n classes)
 """
-def mean_square_error(y_true,y_pred):
+
+
+def mean_square_error(y_true, y_pred):
 
     a = y_pred - y_true
     a_sq = a**2
@@ -27,16 +34,16 @@ def mean_square_error(y_true,y_pred):
 
 
 # y=np.array([0,0,1]) #class #2
- 
+
 # y_pre_good=np.array([0.1,0.1,0.8])
 # y_pre_bed=np.array([0.8,0.1,0.1])
- 
+
 # ce1=cross_entropy(y,y_pre_good)
 # ce2=cross_entropy(y,y_pre_bed)
 
 # mse1=mean_square_error(y,y_pre_good)
 # mse2=mean_square_error(y,y_pre_bed)
- 
+
 # print('Loss 1:',ce1)
 # print('Loss 2:',ce2)
 
--- main_mnist.py
+++ main_mnist.py
@@ -17,6 +17,11 @@ def data_loader():
     # reshape
     X_train = X_train.reshape(len(X_train), 1, 28, 28)
     X_test = X_test.reshape(len(X_test), 1, 28, 28)
+    X_train = X_train.astype("float32")/255  # normalize
+    X_test = X_test.astype("float32")/255  # normalize
+
+    y_train = np_utils.to_categorical(y_train)
+    y_test = np_utils.to_categorical(y_test)
 
     return X_train, y_train, X_test, y_test
 
@@ -24,5 +29,35 @@ def data_loader():
 X_train, y_train, X_test, y_test = data_loader()
 
 print(X_train)
-print(y_train)
+print("train labels:", y_train)
 print(X_train.shape)
+
+
+cnn = [
+    conv_layer((1, 28, 28), 3, 5),
+    Sigmoid(),
+    Reshape((5, 26, 26), (5 * 26*26, 1)),
+    Dense(5*26*26, 60000),
+    Sigmoid(),
+    Dense(60000, 2),
+    Sigmoid()
+]
+
+epochs = 20
+lr = 0.001
+
+
+# train
+for e in range(epochs):
+    error = 0
+    for x, y in zip(X_train, y_train):
+        # forward
+        output = x
+        for layer in cnn:
+            output = layer.forward(output)
+
+        # errors
+        error = cross_entropy(y, output)
+
+        # backward
+        grad =
