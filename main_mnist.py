90c8f49dc3ac2766b6b57128cd9240095ac3d33e
StrangeCroissant <metalocraven@gmail.com>
2023-04-02 21:51:17 +0300

    functional 2nd conv

--- main_mnist.py
+++ main_mnist.py
@@ -5,9 +5,10 @@ from dropout import Dropout
 from dense import Dense
 from conv_layer import Convolutional
 from reshape import Reshape
-from activations import Sigmoid
+from activations import Sigmoid, Softmax, Tanh
 from losses import cross_entropy, cross_entropy_prime
 from cnn import train, predict
+from sklearn.metrics import accuracy_score
 
 
 def preprocess_data(x, y, limit):
@@ -16,17 +17,17 @@ def preprocess_data(x, y, limit):
     # all_indices = np.hstack((zero_index, one_index))
     # all_indices = np.random.permutation(all_indices)
     # x, y = x[all_indices], y[all_indices]
-    x = x.reshape(len(x), 1, 28, 28)
+    x = x.reshape(len(x), 1, 28, 28)[:limit]
     x = x.astype("float32") / 255
-    y = np_utils.to_categorical(y)
+    y = np_utils.to_categorical(y)[:limit]
     y = y.reshape(len(y), 10, 1)
     return x, y
 
 
 # load MNIST from server, limit to 100 images per class since we're not training on GPU
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, y_train = preprocess_data(x_train, y_train, 60000)
-x_test, y_test = preprocess_data(x_test, y_test, 10000)
+x_train, y_train = preprocess_data(x_train, y_train, 600)
+x_test, y_test = preprocess_data(x_test, y_test, 100)
 
 print(len(x_train))
 print(len(x_test))
@@ -35,14 +36,26 @@ print(y_test.shape)
 
 # neural network
 network = [
-    # Dropout(0.5),  # expreimental
-    Convolutional((1, 28, 28), 3, 5),
+    Dropout(0.5),
+    Convolutional((1, 28, 28), 3, 16),
     Sigmoid(),
-    Reshape((5, 26, 26), (5 * 26 * 26, 1)),
-    Dense(5 * 26 * 26, 100),
+
+    Dropout(0.5),
+    Convolutional((16, 26, 26), 3, 32),  # <-- new convolutional layer
     Sigmoid(),
+
+    #Reshape((32, 24, 24), (32 * 24 * 24, 1)),
+    Reshape((32, 24, 24), (32 * 24 * 24, 1)),  # <-- updated reshape layer
+
+    Dropout(0.5),
+    Dense(32 * 24 * 24, 100),
+
+    Sigmoid(),
+    Dropout(0.5),
+
     Dense(100, 10),
-    Sigmoid()
+    # Sigmoid()
+    Softmax()
 ]
 
 # train
@@ -52,8 +65,8 @@ train(
     cross_entropy_prime,
     x_train,
     y_train,
-    epochs=60,
-    learning_rate=0.01
+    epochs=5,
+    learning_rate=0.1
 )
 
 # test
