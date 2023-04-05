d70e9e411183148b894f406c5d6991defe17f966
StrangeCroissant <metalocraven@gmail.com>
2023-04-05 15:46:41 +0300

    a

--- torch_network.py
+++ torch_network.py
@@ -1,8 +1,7 @@
 # generic
-import numpy as np
-import pandas as pd
+
 from datetime import datetime
-# nn layers
+# torch
 import torchvision
 from torch.utils.tensorboard import SummaryWriter
 import torchvision.transforms as transforms
@@ -10,53 +9,8 @@ import torchvision.transforms as transforms
 import torch
 import torch.nn.functional as F
 import torch.nn as nn
-# mnist
-from keras.datasets import mnist
-# utils
-from keras.utils import np_utils
 
 
-# def data_process(x, y, limit):
-#     """
-#     Function that loads and reshapes mnist dataset from keras
-#     limit: limits the dataset length for debuging purposes
-#     """
-
-#     x = x.reshape(len(x), 1, 28, 28)[:limit]
-#     x = x.astype("float32")/255.0
-#     # creating label column vectors of lenght 10
-#     y = np_utils.to_categorical(y)[:limit]
-#     y = y.reshape(len(y), 10, 1)
-
-#     # from np to torch tensors
-
-#     x = torch.from_numpy(x).type(torch.LongTensor)
-#     y = torch.from_numpy(y).type(torch.LongTensor)
-
-#     return x, y
-
-
-# # call the mnist() function and get training validation data
-# (X_train, y_train), (X_test, y_test) = mnist.load_data()
-
-# X_train, y_train = data_process(X_train, y_train, 600)
-# X_test, y_test = data_process(X_test, y_test, 100)
-
-
-# print(
-#     f"Training data shape: {X_train.shape},Training labels shape: {y_test.shape}"
-# )
-# print(
-#     f"Training data type: {type(X_train)},Training labels type: {type(y_test.type)}"
-# )
-
-
-# """
-#  Create dataloades for training testing, batch size can be set here
-# """
-# batch_size = 32
-# train = torch.utils.data.TensorDataset(X_train, y_train)
-# test = torch.utils.data.TensorDataset(X_test, y_test)
 batch_size = 4
 transform = transforms.Compose([transforms.ToTensor(),
                                 transforms.Normalize((0.5,), (0.5,))])
