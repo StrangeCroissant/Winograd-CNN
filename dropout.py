19537b14a0bec3042f0ac1804d89312537fa408b
StrangeCroissant <metalocraven@gmail.com>
2023-04-11 19:26:45 +0300

    tidding up

--- dropout.py
+++ dropout.py
@@ -4,19 +4,17 @@ This is an implementation of a dropout layer
 
 """
 
-from layer import Layer
 import numpy as np
+import torch.nn as nn
 
 
-class Dropout(Layer):
+class Dropout(nn.Module):
     def __init__(self, dropout_rate):
         self.dropout_rate = dropout_rate
         self.mask = None
 
     def forward(self, input):
-
-        self.mask = np.random.binomial(
-            1, 1 - self.dropout_rate, size=input.shape)
+        self.mask = np.random.binomial(1, 1 - self.dropout_rate, size=input.shape)
         # Multiply the input by the mask to "drop out" some values
         self.output = input * self.mask
         return self.output
