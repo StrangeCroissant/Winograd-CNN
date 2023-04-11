47b1d865e6cd5bd70ed7a4c2235fdd6925c84971
StrangeCroissant <metalocraven@gmail.com>
2023-04-11 19:38:15 +0300

    merging files

--- helper_functions.py
+++ helper_functions.py
@@ -1,7 +1,6 @@
-from accuracy_fn import accuracy_fn
 import torch
 import torch.nn as nn
-import torch.optim as optim
+
 import warnings
 from tqdm.auto import tqdm
 import numpy as np
@@ -10,6 +9,8 @@ import numpy as np
 warnings.filterwarnings("ignore")
 # device agnostic script runs in cuda if able
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Model Evaluation function
 
 
 def eval_model(
@@ -52,3 +53,35 @@ def eval_model(
         "model_loss": loss.item(),
         "model_acc": acc,
     }
+
+
+# Accuracy function
+def accuracy_fn(y_true, y_pred):
+    """Calculates accuracy between truth labels and predictions."""
+    correct = torch.eq(y_true, y_pred).sum().item()
+    acc = (correct / len(y_pred)) * 100
+    return acc
+
+
+# cross entropy back and forward (prime)
+
+"""
+    Cross entropy loss function:
+
+        lce = - SUM[i=1 -> n](y_true_i * log(p_i)) for n classes)
+        ex n=10 for mnist 
+        p_i = softmax probability of the ith class or y_pred
+
+        J(x,y) = SUM[m]{y_true*log(y_pred)-
+                    SUM[m]{(1-y_true)log(1-y_pred)}
+"""
+
+
+def cross_entropy(y_true, y_pred):
+    loss = -np.sum(y_true * np.log(y_pred))
+    return loss / float(y_pred.shape[0])
+
+
+def cross_entropy_prime(y_true, y_pred):
+    loss = -np.sum((1 - y_true) * np.log(1 - y_pred))
+    return loss / float(y_pred.shape[0])
