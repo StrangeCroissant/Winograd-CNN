1ddf39b94a8c7eb183bc6afa1d3ceac925c3b29c
StrangeCroissant <metalocraven@gmail.com>
2023-04-11 21:59:56 +0300

    updated

--- tweeking_custom_cnn.py
+++ tweeking_custom_cnn.py
@@ -17,6 +17,9 @@ from helper_functions import accuracy_fn, cross_entropy, cross_entropy_prime
 from custom_layers import ReLU, Dense, Dropout, Reshape
 from winograd2d import WinogradConv2d
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
 batch_size = 4
 train_data = datasets.MNIST(
     root="./data", train=True, download=True, transform=transforms.ToTensor()
@@ -35,6 +38,7 @@ test_loader = torch.utils.data.DataLoader(
     test_data, batch_size=batch_size, shuffle=False
 )
 classes = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+
 
 print(train_loader)
 print(train_data)
@@ -90,103 +94,140 @@ class network(nn.Module):
         return x
 
 
-cnn = network()
+cnn = network().to(device)
 
 print(cnn)
 
 # loss and optimizer
 
 loss_function = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(cnn.parameters(), lr=0.001, momentum=0.9)
+optimizer = torch.optim.SGD(cnn.parameters(), lr=0.01, momentum=0.9)
 
 """
     Training for 1 epoch, we will call this function for every epoch 
 """
 
 
-def epoch_training(epoch_index, tb_writter):
-    running_loss = 0
-    last_loss = 0
+# def epoch_training(epoch_index, tb_writter):
+#     running_loss = 0
+#     last_loss = 0
 
-    for i, data in enumerate(train_loader):
-        # input + label
-        inputs, labels = data
-        # initializing with zero grads
-        optimizer.zero_grad()
-        outputs = cnn(inputs)
-        # calc loss and gradient
-        loss = loss_function(outputs, labels)
-        loss.backward()
-        # adjust weights
-        optimizer.step()
+#     for i, data in enumerate(train_loader):
+#         # input + label
+#         inputs, labels = data
+#         # initializing with zero grads
+#         optimizer.zero_grad()
+#         outputs = cnn(inputs)
+#         # calc loss and gradient
+#         loss = loss_function(outputs, labels)
+#         loss.backward()
+#         # adjust weights
+#         optimizer.step()
 
-        """
-        verbose report
-        """
-        # add loss step to the running loss
-        running_loss += loss.item()
+#         """
+#         verbose report
+#         """
+#         # add loss step to the running loss
+#         running_loss += loss.item()
 
-        if i % 1000 == 999:
-            last_loss = running_loss / 1000  # loss per batch
-            print("batch {} loss: {}".format(i + 1, last_loss))
-            tb_x = epoch_index * len(train_loader) + i + 1
-            tb_writter.add_scalar("Loss/train", last_loss, tb_x)
-            running_loss = 0  # reset loss after each batch
+#         if i % 1000 == 999:
+#             last_loss = running_loss / 1000  # loss per batch
+#             print("batch {} loss: {}".format(i + 1, last_loss))
+#             tb_x = epoch_index * len(train_loader) + i + 1
+#             tb_writter.add_scalar("Loss/train", last_loss, tb_x)
+#             running_loss = 0  # reset loss after each batch
 
-    return last_loss
+#     return last_loss
 
 
-# per epoch training
-timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+# # per epoch training
+# timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-writer = SummaryWriter("runs/mnist_trainer_{}".format(timestamp))
+# writer = SummaryWriter("runs/mnist_trainer_{}".format(timestamp))
 
-epoch_number = 0
+# epoch_number = 0
 
-epochs = 20
+# epochs = 3
 
-best_tloss = 1_000_000.0
+# best_tloss = 1_000_000.0
 
-for epoch in range(epochs):
-    print("epoch {}:".format(epoch_number + 1))
+# for epoch in range(epochs):
+#     print("epoch {}:".format(epoch_number + 1))
 
-    cnn.train(True)
+#     cnn.train(True)
 
-    avg_loss = epoch_training(epoch_number, writer)
+#     avg_loss = epoch_training(epoch_number, writer)
 
-    running_tloss = 0.0
+#     running_tloss = 0.0
 
-    for i, tdata in enumerate(test_loader):
-        tinputs, tlabels = tdata
-        toutputs = cnn(tinputs)
-        # toutputs = loss_function(toutputs, tlabels)
+#     for i, tdata in enumerate(test_loader):
+#         tinputs, tlabels = tdata
+#         toutputs = cnn(tinputs)
+#         # toutputs = loss_function(toutputs, tlabels)
 
-        tloss = loss_function(toutputs, tlabels)
-        running_tloss += tloss
-    avg_tloss = running_tloss / (i + 1)
-    print("Loss train {} test P{}".format(avg_loss, avg_tloss))
+#         tloss = loss_function(toutputs, tlabels)
+#         running_tloss += tloss
+#     avg_tloss = running_tloss / (i + 1)
+#     print("Loss train {} test P{}".format(avg_loss, avg_tloss))
 
-    # log the running loss averaged per batch for train/test
+#     # log the running loss averaged per batch for train/test
 
-    writer.add_scalars(
-        "Trainings vs Testing Loss",
-        {"Training": avg_loss, "Testing": avg_tloss},
-        epoch_number + 1,
+#     writer.add_scalars(
+#         "Trainings vs Testing Loss",
+#         {"Training": avg_loss, "Testing": avg_tloss},
+#         epoch_number + 1,
+#     )
+
+#     writer.flush()
+
+#     # trach best perrformance and save model state
+
+#     if avg_tloss < best_tloss:
+#         best_tloss = avg_tloss
+#         model_path = "model_{}_{}".format(timestamp, epoch_number)
+#         torch.save(cnn.state_dict(), model_path)
+
+#     epoch_number += 1
+
+
+# # load saved model
+
+# # saved_model = cnn()
+# # saved_model.load_state_dict(toch.load(PATH))
+
+
+"""
+Initializing training-testing of nn 
+"""
+train_time_start_on_device = timer()
+
+
+epochs = 3
+for epoch in tqdm(range(epochs)):
+    print(f"Epoch: {epoch}\n-------- ")
+
+    """Training Step """
+    train_step(
+        model=cnn,
+        data_loader=train_loader,
+        loss_fn=loss_function,
+        optimizer=optimizer,
+        accuracy_fn=accuracy_fn,
+        device=device,
     )
 
-    writer.flush()
+    """Testing Step """
+    test_step(
+        model=cnn,
+        data_loader=test_loader,
+        loss_fn=loss_function,
+        accuracy_fn=accuracy_fn,
+        device=device,
+    )
+    print(f"epoch {epoch} done!")
 
-    # trach best perrformance and save model state
 
-    if avg_tloss < best_tloss:
-        best_tloss = avg_tloss
-        model_path = "model_{}_{}".format(timestamp, epoch_number)
-        torch.save(cnn.state_dict(), model_path)
+train_time_end_on_device = timer()
 
-    epoch_number += 1
-
-
-# load saved model
-
-# saved_model = cnn()
-# saved_model.load_state_dict(toch.load(PATH))
+runtime = train_time_end_on_device - train_time_start_on_device
+print(f"Total training runntime:{runtime} second on {device}")
