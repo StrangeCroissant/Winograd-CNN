6d9b59b02845b41a6c8c409d21d90bb56ac6ad5d
StrangeCroissant <metalocraven@gmail.com>
2023-04-17 15:32:50 +0300

    working test tensor fow win

--- winograd2d.py
+++ winograd2d.py
@@ -1,112 +1,68 @@
 import torch
-import torch.nn.functional as F
-
-import math
-
-
-import torch
-import torch.nn.functional as F
+import torch.nn.functional as functional
+import torch.nn as nn
 
 
-class WinogradConv2d(torch.nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0):
+class WinogradConv2d(nn.Module):
+    def __init__(self):
         super(WinogradConv2d, self).__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.kernel_size = kernel_size
-        self.stride = stride
-        self.padding = padding
 
-        # Compute Winograd parameters
-        self.m = 2
-        self.r = 3
-        self.alpha = self.m + self.r - 1
+        """
+        transform_input() : precomputes  B.T x D x B   where B.T a 4x4 winograd domain transform 
+        matrix.
+        For the matrice's definition we will use interpolation points of 
 
-        # Filter transform matrix G
-        self.G = torch.tensor(
-            [
-                [1, 0, 0],
-                [1 / 2, 1 / 2, 1 / 2],
-                [1 / 2, -1 / 2, 1 / 2],
-                [0, 0, 1],
-                [0, 1, 0],
-                [0, -1, 0],
-            ]
-        )
+        """
 
-        # Inverse filter transform matrix GT
-        self.GT = self.G.T
-
-        # Data transform matrix B
         self.B = torch.tensor(
-            [[1, 0, -1, 0], [0, 1, 1, 0], [0, -1, 1, 0], [0, 1, 0, -1]]
+            [[1, 0, -1, 0], [0, 1, 1, 0], [0, -1, 1, 0], [0, 1, 0, -1]],
+            dtype=torch.float32,
         )
+        # self.BT = torch.transpose(self.B, 0, 1)
+        self.BT = self.B.transpose(0, 1).contiguous()
 
-        # Inverse data transform matrix BT
-        self.BT = torch.tensor(
-            [[1, 0, 0, 1], [0, 1, -1, 0], [-1, 1, 1, 0], [0, 0, 0, -1]]
-        )
+        self.AT = torch.tensor([[1, 0, 0, 0], [0, 1, -1, 1]], dtype=torch.float32)
+        # self.AT = torch.transpose(self.A, 0, 1)
+        self.A = torch.tensor([[1, 0], [0, 1], [0, -1], [0, 1]], dtype=torch.float32)
 
-        # Initialize weight and bias tensors
-        self.weight = torch.nn.Parameter(
-            torch.Tensor(out_channels, in_channels, self.m, self.m)
-        )
-        self.bias = torch.nn.Parameter(torch.Tensor(out_channels))
-        self.reset_parameters()
+    def transform_input(self, D):
+        X = self.BT.mul(D).mul(self.B)
+        print("X step shape: ", X.shape[2:4])
+        return X
 
-    def reset_parameters(self):
-        torch.nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
-        if self.bias is not None:
-            fan_in, _ = torch.nn.init._calculate_fan_in_and_fan_out(self.weight)
-            bound = 1 / math.sqrt(fan_in)
-            torch.nn.init.uniform_(self.bias, -bound, bound)
+    def transform_kernel(self, K):
+        K = functional.pad(K, (0, 1, 0, 1), mode="constant")
+        F = torch.zeros((K.shape[0], 4, 4), dtype=torch.float32)
 
-    def forward(self, X):
-        # Pad the input tensor
-        X = F.pad(X, pad=(self.m - 1, self.m - 1, self.m - 1, self.m - 1))
+        F = self.BT.mul(K.mul(self.B))
+        print("Intermidiate kernel shape F :", F.shape)
+        return F
 
-        # Unfold the input tensor into tiles
-        X_tiles = X.unfold(2, self.m, self.r).unfold(3, self.m, self.r)
-        X_tiles = (
-            X_tiles.permute(0, 2, 3, 1, 4, 5)
-            .contiguous()
-            .view(-1, self.m, self.m, self.in_channels)
-        )
-        X = X_tiles
+    def compute_convolution(self, X, F):
+        Y = X[2:4].mul(F[2:4])
+        print("Y shape:", Y.shape)
 
-        # Transform input data to Winograd domain
-        X_tiles = self.B @ X_tiles @ self.BT
+        Z = self.AT.mul(Y[2:4]).mul(self.A)
+        print(Z.shape)
+        return Z
 
-        # Compute output tensor
-        output_tensor = torch.zeros(
-            X.shape[0],
-            self.out_channels,
-            X.shape[2] - self.alpha + 1,
-            X.shape[3] - self.alpha + 1,
-        )
+    def forward(self, X, K):
+        batch_size = X.shape[0]
+        num_filters = K.shape[0]
+        blocks = int(((X.shape[2] - 3) / 2) + 1)  # ex (28-3)/2+1 = 13
 
-        for k in range(self.out_channels):
-            for c in range(self.in_channels):
-                # Compute filter tensor V
-                filter_tensor = (
-                    self.weight[k, c].view(self.m * self.m, -1).transpose(0, 1)
-                )
-                V = self.BT @ filter_tensor @ self.G
+        out_shape = (batch_size, num_filters, blocks, blocks)
 
-                for b in range(X.shape[0]):
-                    for i in range(X_tiles.shape[2]):
-                        for j in range(X_tiles.shape[3]):
-                            # Extract data tensor U from tile
-                            data_tensor = X_tiles[b, c, i, j]
-                            U = self.G @ data_tensor @ self.GT
+        out = torch.zeros(out_shape, dtype=torch.float32)
 
-                            # Compute multiplication tensor M
-                            M = torch.matmul(V, U)
+        for i in range(blocks):
+            for j in range(blocks):
+                tile = X[
+                    :, :, i * 2 : i * 2 + 4, j : j * 2 + 4
+                ]  # 4x4 tile with stride 2
+                X_tile = self.transform_input(tile)
+                F_tile = self.transform_kernel(K)
 
-                            # Reshape and add bias
-                            M = M.view(self.m, self.m, -1).sum(dim=2) + self.bias[k]
+                out[:, :, i, j] = self.compute_convolution(X_tile, F_tile)
 
-                            # Transform back to spatial domain
-                            output_tensor[b, k, i, j] += self.BT @ M @ self.B
-
-        return output_tensor
+        return out
